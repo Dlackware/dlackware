f8ec653b926d79adf04fc2e7f538cdf801d85400
Eugen Wissner <belka@caraus.de>
2019-01-02 08:01:16 +0100

    Report package build errors with ExceptT

--- lib/Slackware/Command.hs
+++ lib/Slackware/Command.hs
@@ -12,18 +12,27 @@ import           CompileOrder ( Step(..)
 import           Config ( Config(..)
                         , parseConfig
                         )
+import Control.Monad.IO.Class (liftIO)
+import           Control.Exception (try)
+import Control.Monad.Trans.Except ( ExceptT(..)
+                                  , runExceptT
+                                  , throwE
+                                  , withExceptT
+                                  )
 import           Crypto.Hash ( hashlazy
                              , Digest
                              , MD5
-                             , digestFromByteString
                              )
 import           Data.Default.Class (def)
 import           Data.Either (fromRight)
+import Data.Foldable (foldrM)
 import qualified Data.ByteString.Char8 as C8
 import qualified Data.ByteString.Lazy as BSL
 import           Data.Maybe (fromJust)
 import qualified Data.Text as T
-import           Network.HTTP.Req (runReq)
+import           Network.HTTP.Req ( HttpException
+                                  , runReq
+                                  )
 import           Slackware.Package ( parseInfoFile
                                    , Package(..)
                                    )
@@ -33,12 +42,14 @@ import System.Directory ( createDirectoryIfMissing
                         , setCurrentDirectory
                         )
 import System.Environment (getEnvironment)
+import System.Exit (ExitCode(..))
 import System.FilePath ( FilePath
                        , (</>)
                        , (<.>)
                        , joinPath
                        , takeDirectory
                        )
+import System.IO.Error (tryIOError)
 import System.Process ( CreateProcess(..)
                       , StdStream(..)
                       , CmdSpec(..)
@@ -48,6 +59,8 @@ import System.Process ( CreateProcess(..)
                       , callProcess
                       )
 import Text.Megaparsec (parse)
+
+type PackageAction = FilePath -> (String, String) -> ExceptT String IO ()
 
 runSlackBuild :: FilePath -> [(String, String)] -> IO CreateProcess
 runSlackBuild slackBuild environment = do
@@ -74,93 +87,121 @@ runSlackBuild slackBuild environment = do
 md5sum :: BSL.ByteString -> Digest MD5
 md5sum = hashlazy
 
-buildPackage :: FilePath -> (String, String) -> IO ()
-buildPackage repo (old, pkgName) = do
-    let infoFile = joinPath [repo, pkgName, pkgName <.> "info"]
-    content <- C8.readFile infoFile
+installpkg :: (String, String) -> Package -> String -> String -> ExceptT String IO ()
+installpkg (old, pkgName) pkg arch buildNumber = withExceptT show $ tryIO callProcess'
+    where
+        tryIO = ExceptT . tryIOError
+        fullPath = "/var/cache/dlackware/" ++ pkgName ++ "-" ++ (version pkg)
+            ++ "-" ++ arch ++ "-" ++ buildNumber ++ "_dlack.txz"
+        callProcess' = callProcess "/sbin/upgradepkg"
+            ["--reinstall", "--install-new", old ++ fullPath]
 
-    let pkg = case parse parseInfoFile infoFile content of
-                  Left left -> error $ show left
-                  Right right -> right
+buildPackage :: PackageAction
+buildPackage repo (old, pkgName) = do
+    liftIO $ putStrLn $ "Building package " ++ pkgName
+
+    let infoFile = joinPath [repo, pkgName, pkgName <.> "info"]
+    content <- liftIO $ C8.readFile infoFile
+
+    pkg <- case parse parseInfoFile infoFile content of
+        Left left -> throwE $ show left
+        Right pkg -> return pkg
 
     let slackBuild = pkgName <.> "SlackBuild"
 
-    oldDirectory <- getCurrentDirectory
-    setCurrentDirectory $ repo </> pkgName
+    oldDirectory <- liftIO $ getCurrentDirectory
+    liftIO $ setCurrentDirectory $ repo </> pkgName
 
     let tarballs = downloads pkg
-    mapM_ ((runReq def) . fromJust . get) tarballs
+
+    downloadUrls
+        <- case foldrM (\x -> ((:) <$> x <*>) . pure) [] (get <$> tarballs) of
+            Nothing -> throwE ""
+            Just downloadUrls -> return downloadUrls
+
+    caught <- liftIO ((try (mapM_ (runReq def) downloadUrls)) :: IO (Either HttpException ()))
+    _ <- case caught of
+        Left e -> throwE $ show e
+        Right unit -> return unit
 
     let filenames = (C8.unpack . snd . (C8.breakEnd ('/' ==))) <$> tarballs
-    sums <- mapM (fmap md5sum . BSL.readFile) filenames
-    if ((Just <$> sums) == (digestFromByteString <$> checksums pkg))
-       then return ()
-       else error "Checksum mismatch"
+    sums <- liftIO $ mapM (fmap md5sum . BSL.readFile) filenames
 
-    (buildNumber, archNoarch) <- grepSlackBuild <$> (readFile slackBuild)
-    unameM <- readProcess "/usr/bin/uname" ["-m"] ""
+    _ <- if (show <$> sums) /= (C8.unpack <$> checksums pkg)
+        then throwE "Checksum mismatch"
+        else return ()
+
+    (buildNumber, archNoarch) <- liftIO $ grepSlackBuild <$> (readFile slackBuild)
+    unameM <- liftIO $ readProcess "/usr/bin/uname" ["-m"] ""
     let arch = if (length archNoarch) > 1
-               then archNoarch
-               else uname unameM
+            then archNoarch
+            else uname unameM
 
-    let fullPath = "/var/cache/dlackware/" ++ pkgName ++ "-" ++ (version pkg)
-                ++ "-" ++ arch ++ "-" ++ buildNumber ++ "_dlack.txz"
+    (_, _, _, processHandle) <- liftIO $ (runSlackBuild slackBuild [("VERSION", version pkg)]) >>= createProcess
+    code <- liftIO $ waitForProcess processHandle
 
-    (_, _, _, processHandle) <- (runSlackBuild slackBuild [("VERSION", version pkg)]) >>= createProcess
-    _ <- waitForProcess processHandle
+    case code of
+        ExitSuccess -> do
+            liftIO $ setCurrentDirectory oldDirectory
+            installpkg (old, pkgName) pkg arch buildNumber
 
-    setCurrentDirectory oldDirectory
+        _ -> throwE ""
 
-    callProcess "/sbin/upgradepkg" ["--reinstall", "--install-new", old ++ fullPath]
-
-installPackage :: FilePath -> (String, String) -> IO ()
+installPackage :: PackageAction
 installPackage repo (old, pkgName) = do
     let infoFile = joinPath [repo, pkgName, pkgName <.> "info"]
-    content <- C8.readFile infoFile
+    content <- liftIO $ C8.readFile infoFile
 
-    let pkg = case parse parseInfoFile infoFile content of
-                  Left left -> error $ show left
-                  Right right -> right
+    pkg <- case parse parseInfoFile infoFile content of
+                Left left -> throwE $ show left
+                Right pkg -> return pkg
 
     let slackBuild = pkgName <.> "SlackBuild"
 
-    oldDirectory <- getCurrentDirectory
-    setCurrentDirectory $ repo </> pkgName
+    oldDirectory <- liftIO getCurrentDirectory
+    liftIO $ setCurrentDirectory $ repo </> pkgName
 
-    (buildNumber, archNoarch) <- grepSlackBuild <$> (readFile slackBuild)
-    unameM <- readProcess "/usr/bin/uname" ["-m"] ""
+    (buildNumber, archNoarch) <- grepSlackBuild <$> (liftIO $ readFile slackBuild)
+    unameM <- liftIO $ readProcess "/usr/bin/uname" ["-m"] ""
     let arch = if (length archNoarch) > 1
-               then archNoarch
-               else uname unameM
+            then archNoarch
+            else uname unameM
 
-    let fullPath = "/var/cache/dlackware/" ++ pkgName ++ "-" ++ (version pkg)
-                ++ "-" ++ arch ++ "-" ++ buildNumber ++ "_dlack.txz"
+    liftIO $ setCurrentDirectory oldDirectory
+    installpkg (old, pkgName) pkg arch buildNumber
 
-    setCurrentDirectory oldDirectory
-
-    callProcess "/sbin/upgradepkg" ["--reinstall", "--install-new", old ++ fullPath]
-
-downloadPackageSource :: FilePath -> (String, String) -> IO ()
+downloadPackageSource :: PackageAction
 downloadPackageSource repo (_, pkgName) = do
     let infoFile = joinPath [repo, pkgName, pkgName <.> "info"]
-    content <- C8.readFile infoFile
+    content <- liftIO $ C8.readFile infoFile
 
-    let pkg = case parse parseInfoFile infoFile content of
-                  Left left -> error $ show left
-                  Right right -> right
+    pkg <- case parse parseInfoFile infoFile content of
+                Left left -> throwE $ show left
+                Right pkg -> return pkg
 
-    setCurrentDirectory $ repo </> pkgName
+    liftIO $ do
+        setCurrentDirectory $ repo </> pkgName
+        mapM_ ((runReq def) . fromJust . get) (downloads pkg)
 
-    mapM_ ((runReq def) . fromJust . get) (downloads pkg)
-
-doCompileOrder :: (FilePath -> (String, String) -> IO ()) -> String -> IO ()
+doCompileOrder :: PackageAction -> String -> IO ()
 doCompileOrder doPackage compileOrder = do
     content <- C8.readFile compileOrder
 
-    mapM_ (doPackage $ takeDirectory compileOrder) (pkgs content)
-        where pkgs content = foldr f [] $ fromRight [] $ parseCompileOrder compileOrder content
-              f (PackageName Nothing new) acc = ("", C8.unpack new) : acc
-              f (PackageName (Just old) new) acc = ((C8.unpack old) ++ "%", C8.unpack new) : acc
+    maybeError <- f $ fromRight [] $ parseCompileOrder compileOrder content
+    case maybeError of
+      Left message -> fail $ "Build errored: " ++ message
+      _ -> return ()
+
+    where
+        f [] = return $ Right ()
+        f (x:xs) = do
+            maybeError <- runExceptT $ packageAction $ explodePackageName x
+            case maybeError of 
+                Left err -> return $ Left err
+                Right _ -> f xs
+        explodePackageName (PackageName Nothing new) = ("", C8.unpack new)
+        explodePackageName (PackageName (Just old) new) = ((C8.unpack old) ++ "%", C8.unpack new)
+        packageAction = doPackage $ takeDirectory compileOrder
 
 getCompileOrders :: IO [FilePath]
 getCompileOrders = do
